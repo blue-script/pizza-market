c62b104239191290f9d2c20a42a171351c319b45
blue-script <alexm92@inbox.ru>
2025-02-22 20:05:22 +0500

    fix: Checkbox component feat: add to Filters -> FilterCheckbox and Price filter

--- components/shared/filters.tsx
+++ components/shared/filters.tsx
@@ -1,5 +1,10 @@
+"use client"
+
 import {cn} from "@/lib/utils"
 import {Title} from "@/components/shared"
+import {FilterCheckbox} from "@/components/shared/filter-checkbox"
+import {Input} from "@/components/ui"
+import {RangeSlider} from "@/components/shared/range-slider"
 
 interface Props {
   className?: string
@@ -11,9 +16,36 @@ export const Filters = (props: Props) => {
     <div className={cn("", className)}>
       <Title text={"Фильтрация"} size={"sm"} className={"mb-5 font-bold"}/>
 
-      <div className={'flex flex-col gap-4'}>
-        <FilterCheckbox text={'Можно собирать'} value={1}/>
-        <FilterCheckbox text={'Новинки'} value={2}/>
+      <div className={"flex flex-col gap-4"}>
+        <FilterCheckbox text={"Можно собирать"} value={"1"}/>
+        <FilterCheckbox text={"Новинки"} value={"2"}/>
+      </div>
+
+      {/* Фильтр цен */}
+      <div className="mt-5 border-y border-y-neutral-100 py-6 pb-7">
+        <p className="font-bold mb-3">Цена от и до:</p>
+        <div className="flex gap-3 mb-5">
+          <Input
+            type="number"
+            placeholder="0"
+            min={0}
+            max={1000}
+            defaultValue={0}
+          />
+          <Input
+            type="number"
+            min={100}
+            max={1000}
+            placeholder="1000"
+          />
+        </div>
+
+        <RangeSlider
+          min={0}
+          max={1000}
+          step={10}
+          value={[0, 1000]}
+        />
       </div>
     </div>
   )
